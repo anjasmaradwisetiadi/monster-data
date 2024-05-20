9160172f2139b5b8f1b08fe8fa9eaee03dfb763e
Anjasmara Dwi Setiadi <enzoblackred@gmail.com>
2024-05-20 22:21:19 +0700

    [development] update url base

--- public/js/app.js
+++ public/js/app.js
@@ -24245,8 +24245,8 @@ var collectionUrl = {
 
   //********** */ for public deployment  (https://master-duel-ruler.site)
   // baseUrl: 'https://master-duel-ruler.site/api/todolist/',
-  'baseUrlHead': 'https://master-duel-ruler.site/',
-  'baseUrlApi': 'https://master-duel-ruler.site/api/'
+  'baseUrlHead': 'https://master-duel-ruler.site',
+  'baseUrlApi': 'https://master-duel-ruler.site/api'
   // 
   // baseUrlApiYgoProDeck: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?'
 };
--- resources/js/urlCollection.js
+++ resources/js/urlCollection.js
@@ -6,8 +6,8 @@ export const collectionUrl={
 
     //********** */ for public deployment  (https://master-duel-ruler.site)
     // baseUrl: 'https://master-duel-ruler.site/api/todolist/',
-    'baseUrlHead': 'https://master-duel-ruler.site/',
-    'baseUrlApi': 'https://master-duel-ruler.site/api/',
+    'baseUrlHead': 'https://master-duel-ruler.site',
+    'baseUrlApi': 'https://master-duel-ruler.site/api',
     // 
     // baseUrlApiYgoProDeck: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?'
 }
