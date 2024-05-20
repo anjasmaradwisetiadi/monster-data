a12f0fffb2230203d5ea73e7ede34e37eb68a515
Anjasmara Dwi Setiadi <enzoblackred@gmail.com>
2024-05-20 21:46:07 +0700

    [development] development on 20/5/2024

--- public/js/app.js
+++ public/js/app.js
@@ -24240,13 +24240,13 @@ __webpack_require__.r(__webpack_exports__);
 var collectionUrl = {
   //********** */ for local deployment 
   // baseUrl: 'http://laravel-vue.test/api/todolist/',
-  'baseUrlHead': 'http://monster-data-server.test',
-  'baseUrlApi': 'http://monster-data-server.test/api'
+  // 'baseUrlHead': 'http://monster-data-server.test',
+  // 'baseUrlApi': 'http://monster-data-server.test/api',
 
   //********** */ for public deployment  (https://master-duel-ruler.site)
   // baseUrl: 'https://master-duel-ruler.site/api/todolist/',
-  // baseUrlHead: 'https://master-duel-ruler.site/',
-  // baseUrlApi: 'https://master-duel-ruler.site/api/',
+  'baseUrlHead': 'https://master-duel-ruler.site/',
+  'baseUrlApi': 'https://master-duel-ruler.site/api/'
   // 
   // baseUrlApiYgoProDeck: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?'
 };
